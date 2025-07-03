415ce8d331d42d82aab686f4d949c13e949c3eb5
UC3M-student <81519967+UC3M-student@users.noreply.github.com>
2025-07-03 14:23:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+# Verificación de Google Search Console
+@app.get("/google512f505ee54497af.html")
+def google_verification():
+    return FileResponse("google512f505ee54497af.html")
+
+# Sitemap
 @app.get("/sitemap.xml")
 def sitemap():
     return FileResponse("static/sitemap.xml", media_type="application/xml")
 
+# Robots.txt
 @app.get("/robots.txt")
 def robots():
     return FileResponse("static/robots.txt", media_type="text/plain")
